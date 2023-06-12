88e450e5c3376b31c3879c929e2eefd4d1b90116
Denis <dzavalskiy@gmail.com>
2023-06-12 21:26:16 +0400

    Four commit

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.delivery.data.DataGenerator.*;
 
 class DeliveryTest {
 
@@ -20,34 +22,10 @@ class DeliveryTest {
         open("http://localhost:9999");
     }
 
+
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
-        //var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysToAddForFirstMeeting = 4;
-        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 7;
-        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-
-        $("[data-test-id=city] input").setValue(generateCity("ru"));
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(firstMeetingDate);
-        $("[data-test-id=name] input").setValue(generateName("ru"));
-        $("[data-test-id=phone] input").setValue(generatePhone("ru"));
-        $("[data-test-id=agreement]").click();
-        $(withText("Запланировать")).click();
-        $(byText("Успешно!")).shouldBe(visible);
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(secondMeetingDate);
-        $(withText("Запланировать")).click();
-        $(byText("Перепланировать")).shouldBe(visible);
-        $(withText("Перепланировать")).click();
-        $(byText("Успешно!")).shouldBe(visible);
-    }
-
-    @Test
-    @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting2() {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
@@ -61,12 +39,14 @@ class DeliveryTest {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(withText("Запланировать")).click();
-        $(byText("Успешно!")).shouldBe(visible);
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate),
+                Duration.ofSeconds(5)).shouldBe(visible);
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(withText("Запланировать")).click();
         $(byText("Перепланировать")).shouldBe(visible);
         $(withText("Перепланировать")).click();
-        $(byText("Успешно!")).shouldBe(visible);
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate),
+                Duration.ofSeconds(5)).shouldBe(visible);
     }
 }
